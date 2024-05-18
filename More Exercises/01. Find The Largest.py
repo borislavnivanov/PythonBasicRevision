7ccd72a5723414ff415caaa82fa54cf19014f057
Borislav Ivanov <borislavnivanov@gmail.com>
2024-05-18 17:42:32 +0300

    update

--- More Exercises/01. Find The Largest.py
+++ More Exercises/01. Find The Largest.py
@@ -1,3 +1,7 @@
-text = list(input())
-text.sort(reverse=True)
-print(*text, sep='')
+# text = list(input())
+# text.sort(reverse=True)
+# print(*text, sep='')
+
+
+text = input()
+print(str.join("", sorted(text, reverse=True)))
